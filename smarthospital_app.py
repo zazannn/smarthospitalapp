97352ea375b37e26f682509adbac7698f1a0e9ef
zazannn <rizan@kodingnext.com>
2026-06-26 13:47:24 +0700

    Update smarthospital_app.py

--- smarthospital_app.py
+++ smarthospital_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import os
  
-st.set_page_config(page_title="Smar Hospital", page_icon="🏥", layout="wide")
+st.set_page_config(page_title="Smart Hospital", page_icon="🏥", layout="wide")
  
 st.markdown("""
 <style>
